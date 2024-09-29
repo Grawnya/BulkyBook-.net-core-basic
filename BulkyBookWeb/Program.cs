d748637d3e0836eb04571d39e72526cac0774880
Grawnya <grainnejdonegan@gmail.com>
2024-09-29 17:14:06 +0200

    Day 2.1: covered up to "MVC Architecture"

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,24 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container e.g. register an email or DB context.
+// The below service is used to create the MVC object. Razor pages would be different.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline - Determines how an app should respond to a web request
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+// These are all types of middleware
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
+//app.UseAuthentication(); // should be used here to check if user is legit - order matters
+app.UseAuthorization(); // can only authorise an authentic user
 
 app.MapControllerRoute(
     name: "default",
